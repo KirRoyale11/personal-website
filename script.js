1a6cdd14810d073d8fdafd25f08efe712f4e54c0
Kirsti Esch <miss.k.esch@gmail.com>
2024-08-28 17:20:13 -0400

    Complete Contacts Links

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+const myForm = document.getElementyById("contactForm");
+
+myForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  formData.forEach((value, key) => {
+    console.log(`${key}: ${value}`);
+  });
+}
+
+// const myForm = document.getElementById("contactForm");
+
+// myForm.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(e) {
+//   e.preventDefault();
+//   const formData = new FormData(e.target);
+//   formData.forEach((value, key) => {
+//     console.log(`${key}: ${value}`);
+//   });
+// }
